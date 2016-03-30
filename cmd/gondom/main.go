e8362cae80613afcabd1a6a3fd5141349db8814f
Qs-F <qsf@mail.queensya.com>
2016-03-30 20:44:05 +0900

    add: support os.Stdout

--- cmd/gondom/main.go
+++ cmd/gondom/main.go
@@ -1,8 +1,43 @@
 package main
 
-import "github.com/Qs-F/gondom"
+import (
+	"bufio"
+	"fmt"
+	"github.com/Qs-F/gondom"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
 
 func main() {
-	println(gondom.Make(21, 123))
-	println(gondom.MakeURL(21, 123))
+	t, err := strconv.Atoi(time.Now().Format("20060102150405"))
+	b := bufio.NewWriter(os.Stdout)
+	if err != nil {
+		log.Print("something wrong")
+		os.Exit(1)
+	}
+	if len(os.Args) >= 2 {
+		n, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Print("Arg is not safe")
+			os.Exit(1)
+		}
+		if len(os.Args) == 3 {
+			if os.Args[2] == "-u" {
+				g := gondom.MakeURL(n, int64(t))
+				// println(g)
+				fmt.Fprintf(b, g)
+				_ = b.Flush()
+			}
+		} else {
+			g := gondom.Make(n, int64(t))
+			// print(g)
+			fmt.Fprintf(b, g)
+			_ = b.Flush()
+		}
+	} else {
+		log.Print("args is not enough")
+		os.Exit(1)
+	}
 }
